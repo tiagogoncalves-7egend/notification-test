2e8326fed2ddb2a4f618e9a7f3cff5c56a6212c8
Tiago Barros <tiago.barros@7egend.cr>
2023-05-01 09:47:22 +0100

    Applies prettier rules to `packages/nodejs/models`.

--- packages/nodejs/models/src/decoders.ts
+++ packages/nodejs/models/src/decoders.ts
@@ -2,12 +2,12 @@ import * as z from 'zod'
 import * as E from 'fp-ts/lib/Either'
 import { pipe } from 'fp-ts/lib/function'
 
-export const parse =
-  <O, I, D extends z.ZodTypeDef = z.ZodTypeDef>(schema: z.ZodSchema<O, D, I>) =>
-  (data: unknown): E.Either<z.ZodError, O> =>
-    pipe(data, schema.safeParse, result =>
-      result.success === false ? E.left(result.error) : E.right(result.data)
-    )
+export const parse = <O, I, D extends z.ZodTypeDef = z.ZodTypeDef>(
+  schema: z.ZodSchema<O, D, I>
+) => (data: unknown): E.Either<z.ZodError, O> =>
+  pipe(data, schema.safeParse, result =>
+    result.success === false ? E.left(result.error) : E.right(result.data)
+  )
 
 export const DateSchema = z.preprocess(arg => {
   if (typeof arg == 'string' || arg instanceof Date) return new Date(arg)
